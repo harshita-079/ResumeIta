8a31560c751025384ec235f40d5dd2744be01507
harshita-079 <harshugupta0220@gmail.com>
2026-07-07 22:40:32 +0530

    change url

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const api=axios.create({
-  baseURL:"http://localhost:5000/api",
+const api = axios.create({
+  baseURL: "https://resumeita-backend.onrender.com/api",
 });
 
 export default api;
